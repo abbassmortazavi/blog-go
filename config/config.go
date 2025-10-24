025138ff688e5f7b0100a52bc5a6710bd3bf1255
Abbass Mortazavi <abbass.mortazavi@patoughi.info>
2025-10-24 12:27:17 +0330

    feat: add viper package to handel configuration

---
+++ config/config.go
@@ -0,0 +1,15 @@
+package config
+
+type Config struct {
+	Host    string
+	Port    string
+	AppName string
+}
+
+type App struct {
+	Name string
+}
+type Server struct {
+	Host string
+	Port string
+}
